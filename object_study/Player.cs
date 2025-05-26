a60e8d96ae2eef5a4dbe49bd2e6ac1fafb5d4fad
LAPTOP-INRA9C6T\USER <yura.ast@gmail.com>
2025-05-26 09:24:37 +0900

    オブジェクト指向の5/23分まで

---
+++ object_study/Player.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace object_study
+{
+    internal class Player
+    {
+        public string name;
+        public int hp =100;
+        public float attack =120;
+        public float defence =80;
+        public string job ="冒険者";
+
+
+        public void Name(string name)
+        {
+            this.name = name;
+        }
+
+        public void Attack() 
+        {
+            Console.WriteLine(attack + "のダメージを与えた");
+        }
+
+        public void StatusDisplay()
+        {
+            Console.WriteLine("NAME:" + name);
+            Console.WriteLine("HP:" + hp);
+            Console.WriteLine("ATK:" + attack);
+            Console.WriteLine("DEF" + defence);
+            Console.WriteLine("JOB"+job);
+        }
+
+    }
+}
---
+++ object_study/Program.cs
@@ -0,0 +1,18 @@
+﻿//複数のキャラクターをたくさん当序させて、それぞれのステータスや特技を順番に表示するプログラム
+
+using object_study;
+
+Player hiroshi = new Player();　//クラスのインスタンス化・実体化
+hiroshi.Name("ひろし");
+hiroshi.Attack();
+hiroshi.StatusDisplay();
+
+Player yoshida = new Player();　//クラスのインスタンス化・実体化
+yoshida.Name("よしだ");
+yoshida.Attack();
+yoshida.StatusDisplay();
+
+Player nanjyolno = new Player();　//クラスのインスタンス化・実体化
+nanjyolno.Name("なんじょる");
+nanjyolno.Attack();
+nanjyolno.StatusDisplay();
