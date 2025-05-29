4360f753f9d6b6b6f0425fc973b474e2bd392d59
LAPTOP-INRA9C6T\USER <yura.ast@gmail.com>
2025-05-29 15:15:56 +0900

    .

--- object_study/Program.cs
+++ object_study/Program.cs
@@ -4,19 +4,35 @@ using object_study;
 
 Player hiroshi = new Warrior("ひろし");　//クラスのインスタンス化・実体化
 //hiroshi.Name("ひろし");
-hiroshi.Action();
-hiroshi.StatusDisplay();
-hiroshi.SpecialAttack();
+//hiroshi.Action();
+//hiroshi.StatusDisplay();
+//hiroshi.SpecialAttack();
 
-Warrior yoshida = new Warrior("よしだ");　//クラスのインスタンス化・実体化
+Warrior yoshida = new Warrior("よしだ");　
 //yoshida.Name("よしだ");
-yoshida.Action(1);
-yoshida.HardAttack();
-yoshida.StatusDisplay();
-yoshida.SpecialAttack();
+//yoshida.Action(1);
+//yoshida.HardAttack();
+//yoshida.StatusDisplay();
+//yoshida.SpecialAttack();
 
-Player nanjolno = new Player();　//クラスのインスタンス化・実体化
-nanjolno.NameSet("なんじょる");
-nanjolno.Action(2);
-nanjolno.StatusDisplay();
-nanjolno.SpecialAttack();
+Player nanjolno = new Player("なんじょる");
+//nanjolno.NameSet("なんじょる");
+//nanjolno.Action(2);
+//nanjolno.StatusDisplay();
+//nanjolno.SpecialAttack();
+
+List<Player> players = new List<Player>();
+players.Add(hiroshi);
+players.Add(yoshida);
+players.Add(nanjolno);
+
+players.RemoveAt(1);
+
+//配列を順番に処理する繰り返し構文
+//抽出する情報の型名　一次的に情報を取り扱う変数名 in 対象配列
+foreach(Player p in players)
+{
+    p.StatusDisplay();
+    p.SpecialAttack();
+}
+
